c2b52e000ae9e3b85c6354ddad59799be482c016
Syed Abdullah <syed.abdullah7638@gmail.com>
2025-03-25 16:13:52 +0500

    Set error messages and Delete button founctinality

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
 var parent = document.getElementById("parent")
+var note = document.getElementById("note")
 
 function getValue() {
     var input = document.getElementById("input");
 
-
-    if(input.value.length <= 5) {
+    //Length Error
+    if (input.value.length <= 4) {
         var input = document.getElementById("input");
         input.style.outline = "1px solid red"
-    }else {
+
+        if (note.children.length == 0) {
+            var p = document.createElement("p")
+            p.innerHTML = "Write atleast 5 characters."
+            p.style.color = "red"
+
+            note.appendChild(p)
+        }else if (note.children.length == 1) {
+            note.removeChild(note.children[0])
+            var p = document.createElement("p")
+            p.innerHTML = "Write atleast 5 characters."
+            p.style.color = "red"
+
+            note.appendChild(p)
+        }
+    } else {
         input.style.outline = "none"
+
+        if (note.children.length == 1) {
+            note.removeChild(note.children[0])
+        }
     }
 
-    if (parent.children.length == 5 ) {
-        var note = document.getElementById("note")
-        var h6 = document.createElement("h6")
-        h6.innerHTML = "Not Enough Space"
-        h6.style.color = "red"
-        
-        note.appendChild(h6)
+    // Storage Error
+    if (parent.children.length == 5 && note.children.length == 0) {
+        var p = document.createElement("p")
+        p.innerHTML = "Not Enough Space"
+        p.style.color = "red"
+        p.setAttribute("class", "storageMsg")
+
+        note.appendChild(p)
     }
 
-    if(input.value.length >= 5 && parent.children.length <= 4) {
+    // Append List item
+    if (input.value.length >= 5 && parent.children.length <= 4) {
         var liElement = document.createElement("li")
-        parent.insertBefore(liElement, parent.children[0])
+        if (parent.children.length >= 1) {
+            parent.insertBefore(liElement, parent.children[0])
+        }else {
+            parent.appendChild(liElement)
+        }
 
         var h4 = document.createElement("h4")
         h4.innerHTML = input.value
@@ -40,8 +66,28 @@ function getValue() {
         var deleteBtn = document.createElement("button")
         deleteBtn.innerHTML = "Delete"
         deleteBtn.setAttribute("class", "delete")
+        deleteBtn.setAttribute("onclick", "deleteval(this)")
         btns.appendChild(deleteBtn)
 
         input.value = ""
+    }
+}
+
+
+function deleteval(btn) {
+    btn.parentElement.parentElement.remove()
+
+    // Remove The existent  Error
+    if (note.children.length == 1) {
+        note.removeChild(note.children[0])
+    }
+
+    // Empty list message
+    if (parent.children.length <= 0) {
+        var p = document.createElement("p")
+        p.innerHTML = "Nothing Is Here..."
+        p.style.color = "red"
+
+        note.appendChild(p)
     }
 }
